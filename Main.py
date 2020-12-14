7f1bafd7b14e8ef74f690f89fb5d776141a6c3af
Tri Abadi <triabadi13@gmail.com>
2020-12-15 00:09:42 +0700

    Second commit

--- Main.py
+++ Main.py
@@ -1419,3 +1419,40 @@ im.show()
 # which python
 # source project1/bin/activate
 # deactivate
+
+# Migrasi environment
+# pip freeze  --local > requirement.txt
+# pindahkan file txt ke environemtn yg diinginkan
+# pip install -r requirement.txt
+
+
+# try and exception
+print("ini adalah program pembagian")
+
+while True:
+    try:
+        import panda
+        penyebut = int(input("masukan angka penyebut: "))
+        pembilang = int(input("masukan angka pembilang: "))
+        hasil = penyebut/pembilang
+        break
+    except ValueError:
+        print("yang anda masukan bukan angka\n")
+    except ZeroDivisionError:
+        print("angka pembilang yang anda masukan adalah nol, pilih yang lain ya bro/sist\n")
+    except ImportError:
+        print("gak ada modulnya bro")
+    except Exception as err:
+        print(err)
+
+"""
+    type of exception errors:
+    1. IOError
+    2. ImportError
+    3. ValueError
+    4. Division by zero
+    5. KeyboardInterupted
+    6. EOFError
+"""
+
+print("berhasil, hasil pembagian adalah: ", hasil)
